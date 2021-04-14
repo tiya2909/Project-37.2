a283610de9a97d581520c2e2ecfb675cb427feeb
tiya2909 <72922131+tiya2909@users.noreply.github.com>
2021-04-14 11:26:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
+
+var FoodGroup, bananaImage;
+var obstaclesGroup, obstacle_img;
+
+var gameOver;
+var score=0;
+var index=0;
+
+
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  player = createSprite(100,340,20,50);
+  
+  ground = createSprite(400,350,800,10);
+  ground.x=ground.width/2;
+  ground.visible=false;
+  
+  FoodGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+}
+
+function draw() {
+  
+  background(255);
+  
+    
+  
+  camera.position.x = displayWidth/2;
+  camera.position.y = player[index-1].y
+  
+    if(FoodGroup.isTouching(player)){
+      FoodGroup.destroyEach();
+    score = score + 2;
+    }
+    switch(score){
+        case 10: player.scale=0.12;
+                break;
+        case 20: player.scale=0.14;
+                break;
+        case 30: player.scale=0.16;
+                break;
+        case 40: player.scale=0.18;
+                break;
+        default: break;
+    }
+  
+    if(keyDown("space") ) {
+      player.velocityY = -12;
+    }
+    player.velocityY = player.velocityY + 0.8;
+  
+    player.collide(ground);
+    spawnFood();
+    spawnObstacles();
+ 
+    if(obstaclesGroup.isTouching(player)){ 
+        player.scale=0.08;
+     // score=score-2;
+    }
+  
+  drawSprites();
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score: "+ score, 500,50);
+}
+
+function spawnFood() {
+  //write code here to spawn the food
+  if (frameCount % 80 === 0) {
+    var banana = createSprite(600,250,40,10);
+    banana.y = random(120,200);    
+    banana.velocityX = -5;
+     //assign lifetime to the variable
+    banana.lifetime = 300;
+    player.depth = banana.depth + 1;
+    
+    //add each banana to the group
+    FoodGroup.add(banana);
+  }
+}
+
+function spawnObstacles() {
+  if(frameCount % 300 === 0) {
+    var obstacle = createSprite(800,350,10,40);
+    obstacle.velocityX = -6;
+    obstacle.lifetime = 300;
+    
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+  }
+}
+
+
+  
+ 
